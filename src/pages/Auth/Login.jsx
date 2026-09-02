28d60bf29aeb7be45da2f3fab12a2305919276be
Hely <shahhely2510@gmail.com>
2026-09-02 18:30:46 +0530

    login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,10 +1,99 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 import "./Auth.css";
+import { supabase } from "../../lib/supabase";
 
-function Login(){
-    return(
-        <>
-            <h1>hello</h1>
-        </>
-    );
+function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    setMessage("");
+    setLoading(true);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    setLoading(false);
+
+    if (error) {
+      setMessage(error.message);
+      return;
+    }
+
+    console.log("Logged in user:", data.user);
+
+    navigate("/dashboard");
+  }
+
+  return (
+    <main className="auth-page">
+      <div className="auth-card">
+        <h1>Welcome back</h1>
+
+        <form className="auth-form" onSubmit={handleLogin}>
+          <div className="input-group">
+            <label>Email</label>
+
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+
+            <div className="password-input">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="••••••••"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+              />
+
+              <button
+                type="button"
+                className="eye-btn"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <EyeOff size={18} />
+                ) : (
+                  <Eye size={18} />
+                )}
+              </button>
+            </div>
+          </div>
+
+          <button className="auth-btn" type="submit" disabled={loading}>
+            {loading ? "Logging in..." : "Log in"}
+          </button>
+        </form>
+
+        {message && <p>{message}</p>}
+
+        <p className="bottom-text">
+          Don't have an account?
+          <Link to="/sign-up"> Create one</Link>
+        </p>
+      </div>
+    </main>
+  );
 }
+
 export default Login;
